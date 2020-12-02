70abab4ffa098e44506acab2e02ac6297dc0924f
Ahmed-aed <aeid767@gmail.com>
2020-12-02 17:56:12 +0200

    test function route relelat  #6

--- src/test/index.integration.test.js
+++ src/test/index.integration.test.js
@@ -1,6 +1,40 @@
-const { getHealthData1 } = require('../controlles/handlers');
+const { getHealthData, getRecipeData } = require("../controlles/handlers");
+const supertest = require("supertest");
+const app = require("../app");
+const pizzaResult = {
+  ENERC_KCAL: { label: "Energy", quantity: 2620.983838835843, unit: "kcal" },
+  PROCNT: { label: "Protein", quantity: 73.06396909756897, unit: "g" },
+  FAT: { label: "Fat", quantity: 70.85852721973856, unit: "g" },
+  SUGAR: { label: "Sugars", quantity: 30.215096388254253, unit: "g" },
+  CHOCDF: { label: "Carbs", quantity: 422.0246006775219, unit: "g" },
+  CHOLE: { label: "Cholesterol", quantity: 108.68666666666667, unit: "mg" },
+  weight: 100,
+};
+test("test post /health", (done) => {
+  supertest(app)
+    .post("/health")
+    .send({
+      value: "pizza",
+    })
+    .set("Content-Type", "application/json")
+    .expect(200)
+    .expect(function (res) {
+      res.body.weight = 100;
+    })
+    .end((err, res) => {
+      if (err) done(err);
+      expect(res.body).toEqual(pizzaResult);
+      done();
+    });
+});
 
-test('test jest', () => {
-	// eslint-disable-next-line no-undef
-	expect(getHealthData1()).toBe(5);
+test("POST /recipe", (done) => {
+  supertest(app)
+    .post("/recipe")
+    .send({ value: "meat" })
+    .expect(200)
+    .end((err, res) => {
+      if (err) return done(err);
+      done();
+    });
 });
